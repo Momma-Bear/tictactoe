f4e4af0c6a1ede32789305e034355b5419bc4174
Bear <b.r.jonasson@gmail.com>
2024-10-04 14:25:21 +0200

    Starting point

---
+++ language.mjs
@@ -0,0 +1,13 @@
+const DICTIONARY = {
+
+    en: {
+        PLAY_AGAIN_QUESTION: "Play again (yes/no)? ",
+        CONFIRM: "y",
+    },
+    no: {
+        PLAY_AGAIN_QUESTION: "Spille igjen (ja/nei)? ",
+        CONFIRM: "j",
+    }
+}
+
+export default DICTIONARY;
